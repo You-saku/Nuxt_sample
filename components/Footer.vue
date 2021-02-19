6f5d5cee4db4d640f5bf861646c27acee902f1f5
You-saku <usakusekine@gmail.com>
2021-02-20 04:12:39 +0900

    Revert "Revert "header_footer""
    This reverts commit 8bd33381cba59569d8fa599cf53628a3fa54c396.

---
+++ components/Footer.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="main">
+    <footer class="footer">
+      Copyright © 2021 You-saku All Rights Reserved.
+    </footer>
+  </div>
+</template>
+
+<style scoped>
+  #main{
+    position: absolute;
+    bottom: 0;
+    width: 100%;
+    padding: 10px;
+
+    background:#2e8b57;
+    text-align: center;
+  }
+</style>
---
+++ components/Nav.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <b-navbar toggleable="lg" type="dark" style="background-color: #93aa7e;">
+      <b-navbar-brand href="/">
+        Home
+      </b-navbar-brand>
+      <b-navbar-nav><!--薄いナビゲーションバー(カーソルでホップするよ)-->
+        <b-nav-item href="#">Profile</b-nav-item>
+        <b-nav-item href="#">Skills</b-nav-item>
+        <b-nav-item href="#">Hobby</b-nav-item>
+        <b-nav-item href="https://jp.vuejs.org/v2/guide/index.html" target="_blank" rel="noopener noreferrer">Vue.js</b-nav-item>
+        <b-nav-item href="https://ja.nuxtjs.org/" target="_blank" rel="noopener noreferrer">Nuxt.js</b-nav-item>
+      </b-navbar-nav>
+
+
+      <b-navbar-nav class="ml-auto">
+        <b-navbar-brand href="https://github.com/You-saku" target="_blank" rel="noopener noreferrer">
+          <img src="@/assets/GitHub-Mark-Light-32px.png">GitHub
+        </b-navbar-brand>
+      </b-navbar-nav>
+
+    </b-navbar>
+  </div>
+</template>
+<style scoped>
+  img{
+    padding: 3px;
+  }
+</style>
